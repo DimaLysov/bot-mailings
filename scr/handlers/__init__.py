3bc81cca38a972bb35f6d16049996932da196cd5
Lysov Dmitrii <dmlysov@mail.ru>
2025-01-10 21:13:26 +0300

    Изменил директорию, изменил сообщение в /help

--- scr/handlers/__init__.py
+++ scr/handlers/__init__.py
@@ -1,9 +1,9 @@
 from handlers.Sample_hd.get_sample_hd import get_sample_router
 from handlers.Sample_hd.set_sample_hd import add_sample_router
 from handlers.Sample_hd.select_sample_hd import select_sample_router
-from .get_email_hd import get_email_router
-from .select_email_hd import select_email_router
-from .set_email_hd import add_email_router
+from handlers.Email_hd.get_email_hd import get_email_router
+from handlers.Email_hd.select_email_hd import select_email_router
+from handlers.Email_hd.set_email_hd import add_email_router
 from .help_hd import help_router
 from .start_hd import start_router
 
